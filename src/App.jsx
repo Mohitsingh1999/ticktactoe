18e5504f4b0883d7c264d312505c7091e06bbd4f
mohit <ms0135048@gmail.com>
2022-03-08 22:10:41 +0530

    Board and Square

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React from 'react';
+import Board from './Components/Board.jsx';
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+const App = () => {
+  return (
+    <div>
+      <h1>TICK TAC TOE</h1>
+      <Board />
+    </div>
+  );
+};
+export default App;
+
+// export default () => (
+//   <>
+//     <h1>Welcome to React Vite Micro App!</h1>
+//     <p>Hard to get more minimal than this React app.</p>
+//   </>
+// );
